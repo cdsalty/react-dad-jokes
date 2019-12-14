488fe9f49196996170fcdc69fde860df93ca0963
cdsalty <soltiscd@gmail.com>
2019-12-14 16:20:24 -0500

    initial commit: recieving response successfully

---
+++ src/JokeList.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class JokeList extends Component {
+  // make api request via axios
+  async componentDidMount() {
+    // the response is coming in the form of HTML so need to change the 'headers' setup
+    let response = await axios.get("https://icanhazdadjoke.com/", {
+      headers: { Accept: "application/json" } // requesting the json version and not the html version
+    });
+    console.log(response); // get an object data and it gives id, a joke and status
+  }
+  render() {
+    return <h2>Dad Jokes</h2>;
+  }
+}
+
+export default JokeList;
+
+/* 
+// https://icanhazdadjoke.com/api
+
+- GET https://icanhazdadjoke.com/ fetch a random dad joke.
+*/
