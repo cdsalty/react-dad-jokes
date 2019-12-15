549a7305f4dd189fe4d1804af05bf9a4678faea9
cdsalty <soltiscd@gmail.com>
2019-12-15 01:19:38 -0500

    getting back 10 jokes, need to list on page

--- src/JokeList.js
+++ src/JokeList.js
@@ -2,17 +2,36 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class JokeList extends Component {
+  static defaultProps = {
+    numJokesToGet: 10
+  };
+  constructor(props) {
+    super(props);
+    this.state = { jokes: [] };
+  }
   // make api request via axios
   async componentDidMount() {
-    // the response is coming in the form of HTML so need to change the 'headers' setup
-    let response = await axios.get("https://icanhazdadjoke.com/", {
-      headers: { Accept: "application/json" } // requesting the json version and not the html version
-    });
-    // console.log(response); // get an object data and it gives id, a joke and status
-    console.log(response.data.joke); // returns ONE joke, issue: plan to render 10 on the page
+    let jokes = []; // fill array and then setState in order to setting state 10 different times
+    // while loop to prevent duplicates
+    while (jokes.length < this.props.numJokesToGet) {
+      let response = await axios.get("https://icanhazdadjoke.com/", {
+        // response is coming back in the form of HTML, need to change the 'headers' setup
+        headers: { Accept: "application/json" } // requesting the json version of the html format coming from response
+      });
+      // console.log(response); // get an object data and it gives id, a joke and status
+      // console.log(response.data.joke); // returns ONE joke but need to render 10 on the page
+      // take the response.data.joke and push it into jokes
+      jokes.push(response.data.joke);
+    }
+    // console.log(jokes);
+    this.setState({ jokes: jokes });
   }
   render() {
-    return <h2>Dad Jokes</h2>;
+    return (
+      <div>
+        <h2>Dad Jokes</h2>
+      </div>
+    );
   }
 }
 
